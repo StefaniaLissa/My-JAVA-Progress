7e230f14ee90e401811eb2385918951d3571f330
Stefy_Lissa <60698993+StefaniaLissa@users.noreply.github.com>
2021-10-26 13:19:35 +0200

    Create Practica_3_5_1.java

---
+++ 3_5/Practica_3_5_1.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Practica_3_5_1{
+	
+    public static void main(String args[]){
+		char caracter;
+		int codigo;
+		
+		for (codigo=40;codigo<=150;codigo++){
+			caracter=(char)codigo;
+			System.out.println("El codigo "+codigo+" se ve como: "+caracter);
+
+		}	
+	}
+}
